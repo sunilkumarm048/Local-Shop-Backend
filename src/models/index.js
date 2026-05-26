ef11e70559cf6393f064c304c1504524516eddfd
sunilkumarm048 <55477491+sunilkumarm048@users.noreply.github.com>
2026-05-26 20:30:09 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@ export { default as User } from './User.js';
 export { default as Shop } from './Shop.js';
 export { default as Category } from './Category.js';
 export { default as Product } from './Product.js';
+export { default as ProductTemplate } from './ProductTemplate.js';
 export { default as Order } from './Order.js';
 export { default as TransportOrder } from './TransportOrder.js';
 export { default as DeliveryProfile } from './DeliveryProfile.js';
