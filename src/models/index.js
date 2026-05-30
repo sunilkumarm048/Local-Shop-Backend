c34c03aef793adb8ab1a8ded8ea9c8bd55b4f4e5
sunilkumarm048 <55477491+sunilkumarm048@users.noreply.github.com>
2026-05-30 19:53:00 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,7 @@ export { default as Category } from './Category.js';
 export { default as Product } from './Product.js';
 export { default as ProductTemplate } from './ProductTemplate.js';
 export { default as Order } from './Order.js';
+export { default as Review } from './Review.js';
 export { default as TransportOrder } from './TransportOrder.js';
 export { default as DeliveryProfile } from './DeliveryProfile.js';
 export { default as WithdrawRequest } from './WithdrawRequest.js';
