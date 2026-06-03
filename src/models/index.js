02c28db95429a8a1ace7854dbdc55a8c5d2c2146
sunilkumarm048 <55477491+sunilkumarm048@users.noreply.github.com>
2026-06-03 21:52:45 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -6,6 +6,7 @@ export { default as ProductTemplate } from './ProductTemplate.js';
 export { default as Order } from './Order.js';
 export { default as Review } from './Review.js';
 export { default as PushSubscription } from './PushSubscription.js';
+export { default as PasswordResetCode } from './PasswordResetCode.js';
 export { default as TransportOrder } from './TransportOrder.js';
 export { default as DeliveryProfile } from './DeliveryProfile.js';
 export { default as WithdrawRequest } from './WithdrawRequest.js';
