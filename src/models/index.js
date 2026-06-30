bbd06766973c90a04605078579c7be4e0ced3489
sunilkumarm048 <55477491+sunilkumarm048@users.noreply.github.com>
2026-06-30 20:55:14 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,7 @@ export { default as Category } from './Category.js';
 export { default as Product } from './Product.js';
 export { default as ProductTemplate } from './ProductTemplate.js';
 export { default as Order } from './Order.js';
+export { default as Booking } from './Booking.js';
 export { default as Review } from './Review.js';
 export { default as PushSubscription } from './PushSubscription.js';
 export { default as PasswordResetCode } from './PasswordResetCode.js';
